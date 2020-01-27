d09f376b70a6692976038431e3d97d23805388cf
AbhishekSanthoshJaya <AbhishekSanthoshJaya@users.noreply.github.com>
2020-01-27 14:13:41 -0500

    Error fixed

--- src/main/java/StudnetMainClass.java
+++ src/main/java/StudnetMainClass.java
@@ -1,24 +1,26 @@
 public class StudnetMainClass {
     public static void main(String[] args) {
     StudnetMainClass s1 = new StudnetMainClass();
-    int[] str = new int[]{4,3,2,6};
+    int[] str = new int[]{3,4,2,3};
     System.out.println(s1.nonDec(str));
 
     }
 
-    public boolean nonDec(int[] arr) {
+    public boolean nonDec(int[] nums) {
         int count = 0;
-        for (int i = 0; i < arr.length-1; i++) {
-                if (arr[i] < arr[i+1]) {
+        for (int i = 0; i < nums.length-1; i++) {
+            for (int j = i + 1; j < nums.length - 1; j++) {
+                if (nums[i] < nums[j]) {
                     return true;
                 }
-                if (arr[i] > arr[i+1]) {
+                else if (nums[i] >= nums[j]) {
                     count++;
                 }
             }
-            if (count >= 2) {
-                return false;
-            }
-            else return true;
+        }
+        if (count >= 2) {
+            return false;
+        }
+        else return true;
     }
 }
